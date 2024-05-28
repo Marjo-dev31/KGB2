12e5152b3bfddd5cacf1a1afa53edd74d20931c2
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-28 15:13:44 +0200

    ejs

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import  express  from "express";
 import  dotenv  from "dotenv";
 
+
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 8000;
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.set("views", "./src");
+
+
+
+
+
+
+app.get('/', (req, res)=> {
+    res.render('index')
+})
 
 app.listen(PORT, ()=> {
     console.log(`It's alive on port ${PORT}`)
