c39d70af834713318af26de16f3a53a130d8e14e
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-28 15:13:44 +0200

    ejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [".scr/**/*.{html,js"],
+  content: ["./src/**/*.{html,js,ejs}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
