c002184a64bd6c19c062686029d1b7e58fb70d26
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-31 18:13:16 +0200

    auhtorization

--- middlewares/authorization.middleware.js
+++ middlewares/authorization.middleware.js
@@ -1,13 +1,15 @@
 const verifyToken = (req,res, next) =>{
     if(req.cookies.jwt){
-      const verify = jwt.verfy(req.cookies.jwt, ACCESS_TOKEN_SECRET)
+      const verify = jwt.verify(req.cookies.jwt, ACCESS_TOKEN_SECRET)
       
       if(!verify){
         res.status(200).render('index.ejs')
       }
       next()
+    } else{
+      res.status(500).render('500.ejs')
     }
-
+    
 }
 
 export default verifyToken
