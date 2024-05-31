facb21c4c39452135daf6b49d96baceb4e266b47
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-05-31 16:47:52 +0200

    compare password test

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,10 +1,11 @@
-import mongoose from 'mongoose';
-import compare from 'bcrypt';
+import {compare} from 'bcrypt';
 import User from '../models/user.model.js'
 
 export const login = async (req,res)=>{
  try {
+  console.log('toto')
    const user = await User.findOne({mail: req.body.mail})
+   console.log(user)
     if(!user){
         res.status(200).send(`User no found`)
         return
@@ -12,8 +13,9 @@ export const login = async (req,res)=>{
    const passwordIsValid = await compare(req.body.password,user.password);
    if(!passwordIsValid){
     res.status(200).send('Please verify your credentials!')
+    return
    }
-   res.status(200).rendre('../src/backoffice.ejs')
+   res.status(200).render('../src/backoffice.ejs')
  } catch(error) {
     res.status(500).render('../src/500.ejs', error.message)
  }
