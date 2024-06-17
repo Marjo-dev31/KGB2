b8a6d57d42e9cf402626bc3abc34f98b031add7a
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-06-17 16:56:27 +0200

    error typo

--- query/mission.query.js
+++ query/mission.query.js
@@ -1,7 +1,7 @@
 const QUERYMISSIONS = {
    SELECT_MISSIONS: 'SELECT * FROM missions',
    SELECT_MISSIONS_DETAILS:
-      'select missions.title,missions.description,missions.codename,missions.status,missions.type,DATE_FORMAT(start_date,"%d/%m/%Y") AS start_date,DATE_FORMAT(end_date,"%d/%m/%Y") AS end_date, agents.lastname AS agent,contacts.lastname AS contact,hideout.codename AS hideout,specialities.name AS expected_speciality,targets.lastname AS target,origins.country AS nationality from missions inner join agents on missions.id_agent=agents.id inner join contacts on missions.id_contact=contacts.id inner join hideout on missions.id_hideout=hideout.id inner join specialities on missions.id_speciality=specialities.id inner join targets on missions.id_target=targets.id inner join origins on missions.id_origin=origins.id',
+      'SELECT missions.title,missions.description,missions.codename,missions.status,missions.type,DATE_FORMAT(start_date,"%d/%m/%Y") AS start_date,DATE_FORMAT(end_date,"%d/%m/%Y") AS end_date, agents.lastname AS agent,contacts.lastname AS contact,hideout.codename AS hideout,specialities.name AS expected_speciality,targets.lastname AS target,origins.country AS nationality FROM missions INNER JOIN agents ON missions.id_agent=agents.id INNER JOIN contacts ON missions.id_contact=contacts.id INNER JOIN hideout ON missions.id_hideout=hideout.id INNER JOIN specialities ON missions.id_speciality=specialities.id INNER JOIN targets ON missions.id_target=targets.id INNER JOIN origins ON missions.id_origin=origins.id',
    CREATE_MISSION:
       'INSERT INTO missions VALUES (DEFAULT,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )',
    UPDATE_MISSION:
